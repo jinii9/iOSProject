71b7f134eea884e8128422052b13324040aecfb7
junsoo1003 <royalex@dgu.ac.kr>
2024-12-19 16:21:34 +0900

    markup: 테이블 뷰

---
+++ miniProject/UIViewExtension.swift
@@ -0,0 +1,49 @@
+//
+//  UIViewExtension.swift
+//  HCar
+//
+//  Created by User on 12/18/24.
+//
+
+import UIKit
+
+extension UIView {
+    
+    @IBInspectable var borderWidth: CGFloat {
+        set {
+            layer.borderWidth = newValue
+        }
+        get {
+            return layer.borderWidth
+        }
+    }
+    
+    @IBInspectable var clipsToBounds: Bool {
+        set {
+            self.clipsToBounds = newValue
+        }
+        get {
+            return self.clipsToBounds
+        }
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat {
+        set {
+            layer.cornerRadius = newValue
+        }
+        get {
+            return layer.cornerRadius
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        set {
+            guard let uiColor = newValue else { return }
+            layer.borderColor = uiColor.cgColor
+        }
+        get {
+            guard let color = layer.borderColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+    }
+}
